aab12d9da195016ac3bd9efa7284e9ffef010325
fanwenxiang326 <fanwenxiang326@163.com>
2021-10-31 14:46:06 +0800

    fix:替换 src 的文件为 vue-admin-template 文件

--- build/webpack.common.config.js
+++ build/webpack.common.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
-  entry: './src/main.js',
+  entry: {
+    main: './src/main.js'
+    // vendor: ['vue', 'vue-router', 'vuex', 'element-ui', 'lodash']
+  },
   module: {
     rules: [
       { // vue 文件
@@ -15,43 +21,115 @@ module.exports = {
           'vue-loader'
         ]
       },
+      { // js 文件
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
       { // 加载 css
         test: /\.css$/,
         use: [
-          'vue-style-loader',
-          'style-loader',
-          'css-loader'
+          // 'vue-style-loader',
+          // MiniCssExtractPlugin.loader, // 如果使用 MiniCssExtractPlugin.loader ，就不能使用 style-loader
+          "style-loader", // Creates `style` nodes from JS strings
+          "css-loader", // Translates CSS into CommonJS
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 'vue-style-loader',
+          // MiniCssExtractPlugin.loader, // 如果使用 MiniCssExtractPlugin.loader ，就不能使用 style-loader
+          "style-loader", // Creates `style` nodes from JS strings
+          "css-loader", // Translates CSS into CommonJS
+          { // Compiles Sass to CSS
+            loader: "sass-loader", 
+            options: {
+              implementation: require("node-sass")
+            }
+          }
         ]
       },
       { // 加载图片
         test: /\.(png|svg|jpg|jpeg|gif)$/,
         use: [
-          'file-loader'
-        ]
+         {
+           loader:  'url-loader',
+           options: {
+            limit: 1,
+            name: 'assets/images/[path]/[name].[hash:7].[ext]'
+          }
+         }
+        ],
+        type: 'javascript/auto'
       }, 
       { // 加载字体
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
-        ]
+          {
+            loader: 'file-loader',
+            options: {
+              name(resourcePath, resourceQuery) {
+                // if (process.env.NODE_ENV === 'development') {
+                //   return '[path][name].[ext]';
+                // }
+     
+                // return '[name].[contenthash].[ext]';
+                return '[path][name].[hash].[ext]'
+              },    
+              outputPath: 'assets/fonts',
+            }
+          }
+        ],
+        type: 'javascript/auto'
       }
     ]
   },
   plugins: [
+    new NodePolyfillPlugin(),  // Polyfill Node.js core modules in Webpack. This module is only needed for webpack 5+.
     new  VueLoaderPlugin(),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'assets/style/[name][hash].css'
+    }),
     new HtmlWebpackPlugin({
       title: '开发环境',
       template: path.resolve(__dirname, '../public/index.html'), // 源模板文件
       filename: './index.html', // 输出文件【注意：这里的根路径是module.exports.output.path】
-    })
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+           from: path.resolve(__dirname, '../public'),
+            globOptions: {
+              ignore: ["**/index.html"],
+            }
+        }
+      ],
+    }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'assets/js/[name].bundle.js',
     path: path.resolve(__dirname, '../dist')
   },
   resolve: {
     // 配置 省略文件后缀名
-    extensions: ['.js', '.css', '.vue'],  
-  }
+    extensions: ['.js', '.vue'],  
+    alias: {
+      '@': path.resolve(__dirname, '../src')
+    }
+  },
+  externals: {
+    vue: 'Vue'
+  },
+  optimization: {
+    splitChunks: {
+        chunks: 'all'
+    }
+  },
+  devtool: 'source-map'
 }
--- build/webpack.development.config.js
+++ build/webpack.development.config.js
@@ -3,6 +3,10 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common.config')
 const developmentConfig =  {
   mode: 'development',
+  devServer: {
+        port: 9091
+  },
+  devtool: 'source-map'
 }
 
 const webpackConfig = merge(commonConfig, developmentConfig);
